a7b49476fa400f109ef906303ac5002fa1bbf80c
naijamesz <kodlikejamesz@hotmail.com>
2024-06-16 06:26:12 +0700

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,32 @@
-import { Elysia } from "elysia";
+import { Elysia } from 'elysia';
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+type Book = {
+  id: number;
+  title: string;
+  price: number;
+};
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+let bookList: Book[] = [
+  { id: 1, title: 'Harry Potter', price: 100 },
+  { id: 2, title: 'Lord of the Rings', price: 150 },
+  { id: 3, title: 'Game of Thrones', price: 200 },
+  { id: 4, title: 'The Witcher', price: 250 },
+  { id: 5, title: 'Clean Code', price: 300 },
+  { id: 6, title: 'Refactoring', price: 350 },
+  { id: 7, title: 'Design Patterns', price: 400 },
+  { id: 8, title: 'Code Complete', price: 450 },
+  { id: 9, title: 'The Pragmatic Programmer', price: 500 },
+];
+
+const app = new Elysia()
+  .get('/', () => 'Hello Elysia')
+  .get('/books', () => bookList)
+  .post('/books', ({ body }) => {
+    const newBook = body as Book;
+    newBook.id = bookList.length + 1;
+    bookList.push(newBook);
+    return newBook;
+  })
+  .listen(3000);
+
+console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
